ae8f46e135cf40d267fadecebe130a7b1a160e9d
Piotr Kowalski <piecioshka@gmail.com>
2018-09-10 14:12:13 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,25 @@
+# react-bounty
+
+[![npm version](https://badge.fury.io/js/react-bounty.svg)](https://badge.fury.io/js/react-bounty)
+[![downloads count](https://img.shields.io/npm/dt/react-bounty.svg)](https://www.npmjs.com/~piecioshka)
+[![dependencies](https://david-dm.org/piecioshka/react-bounty.svg)](https://github.com/piecioshka/react-bounty)
+
+[Bounty](https://github.com/coderitual/bounty) React Component
+
+## Installation
+
+```bash
+npm install react-bounty
+```
+
+## Usage
+
+```javascript
+import ReactBounty from 'react-bounty';
+
+<Bounty value={ `${speakers}` } className="bounty"></Bounty>
+```
+
+## License
+
+[The MIT License](http://piecioshka.mit-license.org) @ 2018
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import bounty from 'bounty';
+
+export default class ReactBounty extends PureComponent {
+    // Information about options can be found here:
+    // https://github.com/coderitual/bounty
+    static propTypes = {
+        value: PropTypes.string.isRequired,
+        initialValue: PropTypes.string,
+        lineHeight: PropTypes.number,
+        letterSpacing: PropTypes.number,
+        animationDelay: PropTypes.number,
+        letterAnimationDelay: PropTypes.number,
+    };
+
+    constructor(props) {
+        super(props);
+        this.node = React.createRef();
+    }
+
+    componentDidMount() {
+        const { value, ...options } = this.props;
+        bounty({
+            el: this.node.current,
+            value,
+            ...options,
+        });
+    }
+
+    render() {
+        return React.createElement('div', {
+            className: this.props.className,
+            ref: this.node,
+        });
+    }
+}
